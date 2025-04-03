faef68959e54414088232efd6789dca0a4b05eed
Gersoncs107 <gerson.silva107@gmail.com>
2025-04-02 23:00:45 -0300

    UsernamesAdd

--- db/queries.js
+++ db/queries.js
@@ -4,3 +4,12 @@ async function getAllUsernames() {
     const {rows} = await pool.query("SELECT * FROM USERNAMES")
     return rows
 }
+
+async function insertUsername(username) {
+    await pool.query("INSERT INTO usernames (username) VALUES ($1)", [username])
+}
+
+module.exports = {
+    getAllUsernames,
+    insertUsername
+}
