5eb080dd2fcbb21f331b2929604dcc48ebfa3683
Gersoncs107 <gerson.silva107@gmail.com>
2025-04-03 23:08:55 -0300

    UsernamesAdd

--- db/pool.js
+++
@@ -1,9 +0,0 @@
-const {Pool} = require("pg");
-
-module.exports = new Pool({
-    host: "localhost",
-    user: "postgres", // Replace with your PostgreSQL username
-    database: "top_users",
-    password: "32710019numB$", // Replace with your PostgreSQL password
-    port: 5432
-});
